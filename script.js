cf07aa1725d39b03b060cfffd2aa96aaa4685d46
Trevor Johnson <trevorjohnson9601@gmail.com>
2022-06-06 09:07:17 -0400

    Fix more bugs. Format output. Take off trailing decimal points in last display

--- script.js
+++ script.js
@@ -23,38 +23,99 @@ equals.addEventListener("click", () => {
 numbers.forEach((number) => number.addEventListener("click", updateCurrDisplay));
 operations.forEach((operation) => operation.addEventListener("click", updateOperation))
 function updateCurrDisplay() {
-    if(gotAnswer){
+    if (gotAnswer) {
+        if (this.textContent === ".") {
+            if (firstOperand === "-") {
+                currDisplay.textContent = "-0."
+                firstOperand = currDisplay.textContent;
+                gotAnswer = false;
+                return;
+            }
+            else {
+                currDisplay.textContent = "0.";
+                firstOperand = currDisplay.textContent;
+                gotAnswer = false;
+                return;
+            }
+        }
         currDisplay.textContent = "";
         gotAnswer = false;
     }
     if (firstOperand !== null && operation && currDisplay.textContent.length < 8) {
+        if (secondOperand === null && this.textContent.includes(".")) {
+            currDisplay.textContent = "0.";
+            secondOperand = currDisplay.textContent;
+        }
+        else if (secondOperand === "-" && this.textContent.includes(".")) {
+            currDisplay.textContent = "-0.";
+            secondOperand = currDisplay.textContent;
+        }
+        if (this.textContent === "." && secondOperand.includes(".")) {
+            return;
+        }
         currDisplay.textContent += this.textContent;
         secondOperand = currDisplay.textContent;
     }
     else if (currDisplay.textContent === "0") {
+        if (this.textContent === ".") {
+            currDisplay.textContent = "0.";
+            firstOperand = currDisplay.textContent;
+            return;
+        }
         currDisplay.textContent = this.textContent;
         firstOperand = currDisplay.textContent;
     }
+    else if (currDisplay.textContent === "-") {
+        if (this.textContent === ".") {
+            currDisplay.textContent = "-0.";
+            firstOperand = currDisplay.textContent;
+            return;
+        }
+        currDisplay.textContent += this.textContent;
+        firstOperand = currDisplay.textContent;
+    }
     else if (currDisplay.textContent.length < 8) {
+        if (this.textContent === "." && firstOperand.includes(".")) {
+            return;
+        }
         currDisplay.textContent += this.textContent;
         firstOperand = currDisplay.textContent;
     }
 }
 function updateOperation() {
-    if (firstOperand !==null && secondOperand!== null) {
-        firstOperand = executeEval(operation, firstOperand, secondOperand);
-        secondOperand = null;
-        currDisplay.textContent = "";
+    if (firstOperand === "ERROR") {
+        firstOperand = "0";
+        currDisplay.textContent = "0";
+        return;
     }
-    else if(firstOperand === null && this.textContent === "-"){
+    if (firstOperand === null && this.textContent === "-") {
         firstOperand = "-";
         currDisplay.textContent = "-";
         return;
     }
-    else if(operation !== null && secondOperand === null && this.textContent === "-"){
+    else if (firstOperand === null) {
+        firstOperand = "0";
+    }
+    else if (firstOperand === "-") {
+        return;
+    }
+    if (firstOperand !== null && secondOperand !== null) {
+        let answer = executeEval(operation, firstOperand, secondOperand);
+        firstOperand = answer;
+        secondOperand = null;
+    }
+    else if (firstOperand === null && this.textContent === "-") {
+        firstOperand = "-";
+        currDisplay.textContent = "-";
+        return;
+    }
+    else if (operation !== null && secondOperand === null && this.textContent === "-") {
         secondOperand = "-";
         currDisplay.textContent = "-";
         return;
+    }
+    if (firstOperand.charAt(firstOperand.length - 1) === ".") {
+        firstOperand = firstOperand.slice(undefined, firstOperand.length - 1);
     }
     currDisplay.textContent = "";
     operation = this.textContent;
@@ -68,23 +129,48 @@ function executeClear() {
     operation = null;
 }
 function add(a, b) {
-    return a + b;
+    let answer = String(Math.floor((a + b) * 1000000000000000) / 1000000000000000);
+    if (answer.length > 14) {
+        String(answer = Number.parseFloat(answer).toExponential(7));
+    }
+    return answer;
 }
 function subtract(a, b) {
-    return a - b;
+    let answer = String(Math.floor((a - b) * 1000000000000000) / 1000000000000000);
+    if (answer.length > 14) {
+        String(answer = Number.parseFloat(answer).toExponential(7));
+    }
+    return answer;
 }
 function multiply(a, b) {
-    return a * b;
+    let answer = String(Math.floor((a * b) * 1000000000000000) / 1000000000000000);
+    if (answer.length > 14) {
+        String(answer = Number.parseFloat(answer).toExponential(7));
+    }
+    return answer;
 }
 function divide(a, b) {
-    return a / b;
+    let answer = String(Math.floor((a / b) * 1000000000000000) / 1000000000000000);
+    if (answer.length > 14) {
+        String(answer = Number.parseFloat(answer).toExponential(7));
+    }
+    return answer;
 }
 function executeEval(operation, firstOperand, secondOperand) {
     if (secondOperand === null) {
+        if (firstOperand.charAt(firstOperand.length - 1) === ".") {
+            firstOperand = firstOperand.slice(undefined, firstOperand.length - 1);
+        }
         return firstOperand;
     }
-    a = Number.parseInt(firstOperand);
-    b = Number.parseInt(secondOperand);
+    if (firstOperand.includes(".") || secondOperand.includes(".")) {
+        a = Number.parseFloat(firstOperand);
+        b = Number.parseFloat(secondOperand);
+    }
+    else {
+        a = Number.parseInt(firstOperand);
+        b = Number.parseInt(secondOperand);
+    }
     switch (operation) {
         case "+": return add(a, b);
         case "-": return subtract(a, b);
